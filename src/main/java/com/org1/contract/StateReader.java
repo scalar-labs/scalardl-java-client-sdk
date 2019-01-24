2b354ba657d3580972264fdff55407b36ae958e5
Hiroyuki Yamada <mogwaing@gmail.com>
2019-01-24 17:54:55 +0900

    fix a bug in null prev-hash

--- src/main/java/com/org1/contract/StateReader.java
+++ src/main/java/com/org1/contract/StateReader.java
@@ -7,6 +7,7 @@ import com.scalar.ledger.ledger.Ledger;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -20,15 +21,19 @@ public class StateReader extends Contract {
     InternalAsset internal = (InternalAsset) asset.get();
 
     Base64.Encoder encoder = Base64.getEncoder();
-    return Json.createObjectBuilder()
+    JsonObjectBuilder builder = Json.createObjectBuilder()
         .add("age", internal.age())
         .add("input", internal.input())
         .add("output", internal.data())
         .add("contract_id", internal.contractId())
         .add("argument", internal.argument())
         .add("signature", encoder.encodeToString(internal.signature()))
-        .add("prev_hash", encoder.encodeToString(internal.prevHash()))
-        .add("hash", encoder.encodeToString(internal.hash()))
-        .build();
+        .add("hash", encoder.encodeToString(internal.hash()));
+    if (internal.prevHash() != null) {
+      builder.add("prev_hash", encoder.encodeToString(internal.prevHash()));
+    } else {
+      builder.add("prev_hash", "");
+    }
+    return builder.build();
   }
 }
