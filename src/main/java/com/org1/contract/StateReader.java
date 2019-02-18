b81c9aee940d9046de0cd98cffaea4e334024db7
Hiroyuki Yamada <mogwaing@gmail.com>
2019-02-18 14:14:45 +0900

    Proper error handling (#4)
    * update contracts to handle errors properly

--- src/main/java/com/org1/contract/StateReader.java
+++ src/main/java/com/org1/contract/StateReader.java
@@ -5,22 +5,23 @@ import com.scalar.ledger.asset.InternalAsset;
 import com.scalar.ledger.contract.Contract;
 import com.scalar.ledger.exception.ContractContextException;
 import com.scalar.ledger.ledger.Ledger;
-
+import java.util.Base64;
+import java.util.Optional;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-import java.util.Base64;
-import java.util.Optional;
 
 public class StateReader extends Contract {
 
   @Override
   public JsonObject invoke(Ledger ledger, JsonObject argument, Optional<JsonObject> properties) {
-    String assetId = argument.getString("asset_id");
-    if (assetId == null) {
+    if (!argument.containsKey("asset_id")) {
+      // ContractContextException is the only throwable exception in a contract and
+      // it should be thrown when a contract faces some non-recoverable error
       throw new ContractContextException("please set asset_id in the argument");
     }
 
+    String assetId = argument.getString("asset_id");
     Optional<Asset> asset = ledger.get(assetId);
     if (!asset.isPresent()) {
       return null;
@@ -28,14 +29,15 @@ public class StateReader extends Contract {
     InternalAsset internal = (InternalAsset) asset.get();
 
     Base64.Encoder encoder = Base64.getEncoder();
-    JsonObjectBuilder builder = Json.createObjectBuilder()
-        .add("age", internal.age())
-        .add("input", internal.input())
-        .add("output", internal.data())
-        .add("contract_id", internal.contractId())
-        .add("argument", internal.argument())
-        .add("signature", encoder.encodeToString(internal.signature()))
-        .add("hash", encoder.encodeToString(internal.hash()));
+    JsonObjectBuilder builder =
+        Json.createObjectBuilder()
+            .add("age", internal.age())
+            .add("input", internal.input())
+            .add("output", internal.data())
+            .add("contract_id", internal.contractId())
+            .add("argument", internal.argument())
+            .add("signature", encoder.encodeToString(internal.signature()))
+            .add("hash", encoder.encodeToString(internal.hash()));
     if (internal.prevHash() != null) {
       builder.add("prev_hash", encoder.encodeToString(internal.prevHash()));
     } else {
--- src/main/java/com/org1/contract/StateUpdater.java
+++ src/main/java/com/org1/contract/StateUpdater.java
@@ -1,17 +1,23 @@
 package com.org1.contract;
 
-import com.scalar.ledger.contract.Contract;
-import com.scalar.ledger.ledger.Ledger;
 import com.scalar.ledger.asset.Asset;
-
+import com.scalar.ledger.contract.Contract;
+import com.scalar.ledger.exception.ContractContextException;
+import com.scalar.ledger.ledger.Ledger;
+import java.util.Optional;
 import javax.json.Json;
 import javax.json.JsonObject;
-import java.util.Optional;
 
 public class StateUpdater extends Contract {
 
   @Override
   public JsonObject invoke(Ledger ledger, JsonObject argument, Optional<JsonObject> properties) {
+    if (!argument.containsKey("asset_id") || !argument.containsKey("state")) {
+      // ContractContextException is the only throwable exception in a contract and
+      // it should be thrown when a contract faces some non-recoverable error
+      throw new ContractContextException("please set asset_id and state in the argument");
+    }
+
     String assetId = argument.getString("asset_id");
     int state = argument.getInt("state");
 
